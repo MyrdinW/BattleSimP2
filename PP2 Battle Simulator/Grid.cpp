c79c95300061b8336f8014dcf2924335936f5d49
Myrdin <myrdin.willemse@student.nhlstenden.com>
2021-03-24 15:24:41 +0100

    Grid

--- PP2 Battle Simulator/Grid.cpp
+++ PP2 Battle Simulator/Grid.cpp
@@ -1,1 +1,57 @@
-#include "precomp.h";
+#include "Grid.h"
+#include "defines.h"
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+using namespace Tmpl8;
+
+Grid* Grid::instance = nullptr;
+mutex mtx2;
+
+Grid::Grid()
+{
+    for (auto& x : grid)
+        for (auto& y : x) y.reserve(500);
+}
+
+Grid::~Grid() = default;
+
+Grid* Grid::Instance()
+{
+    if (instance == nullptr) instance = new Grid();
+    return instance;
+}
+
+
+vector<vec2> Grid::GetNeighbouringCells()
+{
+    vector<vec2> cells = {
+        {0, 0},
+        {0, 1},
+        {1, 1},
+        {1, 0},
+        {1, -1},
+        {0, -1},
+        {-1, -1},
+        {-1, 0},
+        {-1, 1} };
+    return cells;
+}
+
+void Grid::AddTankToGridCell(Tank * tank) { grid[tank->gridCell.x][tank->gridCell.y].emplace_back(tank); }
+
+void Grid::MoveTankToGridCell(Tmpl8::Tank * tank, const vec2&newPos)
+{
+    //scoped_lock lock(mtx2);
+    auto& gridCell = grid[tank->gridCell.x][tank->gridCell.y];
+    grid[newPos.x][newPos.y].emplace_back(tank);
+    for (int i = 0; i < gridCell.size(); ++i)
+    {
+        if (gridCell[i] == tank)
+        {
+            gridCell.erase(gridCell.begin() + i);
+            break;
+        }
+    }
+}
--- PP2 Battle Simulator/Grid.h
+++ PP2 Battle Simulator/Grid.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include "defines.h"
+#include "tank.h"
+#include <vector>
+#include "template.h"
+
+namespace Tmpl8
+{
+    class Grid
+    {
+    public:
+        static Grid* Instance();
+        ~Grid();
+        void AddTankToGridCell(Tank* tank);
+        static vec2 GetGridCell(const vec2& position);
+        void MoveTankToGridCell(Tank* tank, const vec2& newPos);
+        static std::vector<vec2> GetNeighbouringCells();
+
+        std::vector<Tank*> grid[GRID_SIZE + 1][GRID_SIZE + 1];
+
+    private:
+        /* Here will be the instance stored. */
+        static Grid* instance;
+
+        /* Private constructor to prevent instancing. */
+        Grid();
+    };
+}
