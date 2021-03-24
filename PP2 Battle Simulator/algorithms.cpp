8ffda1e332ed73ad7430265c5358ce1662acc293
w-hulshoff <whulshoff@gmail.com>
2021-03-24 14:33:42 +0100

    algorithms

---
+++ PP2 Battle Simulator/algorithms.cpp
@@ -0,0 +1,57 @@
+﻿#include "Algorithms.h"
+#include <cmath>
+
+#ifdef USING_EASY_PROFILER
+#include <easy/profiler.h>
+#endif
+
+using namespace std;
+
+namespace PP2
+{
+vector<int> CountSort(const vector<Tank*>& in)
+{
+    vector<int> Counters(TANK_MAX_HEALTH + 1, 0);
+    vector<int> Results;
+
+    for (auto x : in)
+        Counters.at(x->health <= 0 ? 0 : x->health)++;
+
+    for (int i = 0; i < TANK_MAX_HEALTH + 1; ++i)
+        if (Counters[i] != 0)
+            for (int y = 0; y < Counters[i]; ++y)
+                Results.push_back(i);
+
+    return Results;
+}
+
+template <class T>
+void LinkedList<T>::InsertValue(T value)
+{
+    auto* new_node = new Node<T>(value);
+
+    if (head == nullptr || value <= head->value)
+    {
+        new_node->next = head;
+        head = new_node;
+        return;
+    }
+
+    Node<T>* current = head;
+    while (current->next != nullptr && value >= current->next->value) { current = current->next; }
+
+    //Add node
+    new_node->next = current->next;
+    current->next = new_node;
+}
+
+// -----------------------------------------------------------
+// Sort tanks by health value using bucket sort
+// -----------------------------------------------------------
+template <>
+vector<LinkedList<int>> LinkedList<int>::Sort(vector<Tank*>& input, int n_buckets)
+{
+    vector<LinkedList<int>> buckets(n_buckets);
+    for (auto tank : input) { buckets.at(tank->health / n_buckets).InsertValue(tank->health); }
+    return buckets;
+}
---
+++ PP2 Battle Simulator/algorithms.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+
+#include "Grid.h"
+#include "defines.h"
+#include "explosion.h"
+#include "particle_beam.h"
+#include "rocket.h"
+#include "smoke.h"
+#include "tank.h"
+#include <cstdint>
+#include <iostream>
+
+namespace PP2
+{
+/**
+ * Node Used by the Bucketsort/LinkedList
+ * @tparam T
+ */
+template <class T>
+class Node
+{
+  public:
+    Node() : value(nullptr), next(nullptr) {}
+
+    Node(T value) : value(value), next(nullptr) {}
+
+    ~Node()
+    {
+        if (next != nullptr) delete next;
+    }
+
+    T value;
+    Node<T>* next;
+};
+
+ /*
+ * Bucketsort/LinkedList
+ * @tparam T
+ */
+template <class T>
+class LinkedList
+{
+  public:
+    LinkedList() : head(nullptr){};
+
+    ~LinkedList()
+    {
+        delete head;
+    }
+    /**
+ * Insert a value into the list
+ * @param value Value to insert
+ */
+    void InsertValue(T value);
+
+    /**
+ * Sort the list
+ * @param input List to sort
+ * @param n_buckets Number of buckets to use for sorting
+ * @return Sorted list
+ */
+    static std::vector<LinkedList<T>> Sort(std::vector<Tank*>& input, int n_buckets);
+
+    /**
+ * The head
+ */
+    Node<T>* head;
+};
